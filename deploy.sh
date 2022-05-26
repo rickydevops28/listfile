b4e5c77b1a9cfb17ff9cc7799e4a61850e464964
Simon Legah <legah2000@gmail.com>
2022-05-26 04:28:55 +0000

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,12 @@ deploy java application
 Deploy pyhton apps
 deploy in th dev env
 deployment5 master
+<<<<<<< HEAD
 deployment4
 deploy to staging environment 
 echo good job
 Tested and accepted
 gii
 simon
-peter
+depolu 
 
